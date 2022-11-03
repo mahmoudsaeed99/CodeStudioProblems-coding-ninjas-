60e28e7c7667750ef0192937fd4dd004c7bc412e
MahmoudSaeed <saeedmahmoud389@gmail.com>
2022-11-03 19:41:21 +0200

    add Maximum sum rectangle

--- src/MaximumSubarraySum.java
+++ src/MaximumSubarraySum.java
@@ -1,18 +1,22 @@
 public class MaximumSubarraySum {
     public static void main(String[] args) {
         int sum = 0;
-        int maxSum = -1;
-        int[] arr = new int[]{1,2,7,-4,3,2,-10,9,1};
+        int maxSum = -Integer.MAX_VALUE;
+        int[] arr = new int[]{1,-2,-3,4,-1,2,1};
         int n = arr.length;
+        int start = 0;
+        int end = 0;
         for(int i = 0 ; i < n ; i++){
             sum = sum + arr[i];
+             if(sum > maxSum){
+                 maxSum = sum;
+                 end = i;
+            }
             if(sum < 0){
+                start = i+1;
                 sum = 0;
             }
-            if(sum > maxSum){
-                maxSum = sum;
-            }
         }
-        System.out.println(maxSum);
+        System.out.println(start+" to "+end+"    "+maxSum);
     }
 }
