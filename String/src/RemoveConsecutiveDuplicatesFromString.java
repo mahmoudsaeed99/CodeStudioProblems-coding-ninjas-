eb0addb8ac96bf1bc1365f0cea8f9fb611493a1f
MahmoudSaeed <saeedmahmoud389@gmail.com>
2022-12-20 15:53:06 +0200

    added

---
+++ String/src/RemoveConsecutiveDuplicatesFromString.java
@@ -0,0 +1,21 @@
+public class RemoveConsecutiveDuplicatesFromString {
+    public static String removeConsecutive(String str)
+    {
+        String s = "";
+        int i = 0;
+        while(i < str.length()-1){
+            if (Character.toLowerCase(str.charAt(i)) != Character.toLowerCase(str.charAt(i+1))){
+                s += str.charAt(i)+"";
+            }
+            i++;
+        }
+        s += str.charAt(i)+"";
+        return s;
+
+    }
+    public static void main(String[] args) {
+        String s = "xxxyyyzwwzzz";
+        String r = removeConsecutive(s);
+        System.out.println(r);
+    }
+}
